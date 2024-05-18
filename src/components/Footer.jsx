6b9fc41af0ca78e8134be888b5c29053aaa4b7be
Alessandro1809 <dalessandro18dg@gmail.com>
2024-05-17 19:30:11 -0600

    footer terminado

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,9 +2,10 @@ const Footer = () => {
   return (
     <>
     <footer className="">
-      <div>
-      <p>Copyright 2024. All rights reserved.</p>
-
+      <div className="grid justify-center text-center py-10">
+      <h2 className="text-3xl font-semibold max-w-screen-xl mt-8 tracking-wider">ALLRECIPES</h2>
+      <p className="font-medium tracking-wide">Copyright 2024. All rights reserved.</p>
+      <p className="font-medium tracking-wide">Made by C02623-Alessandro Díaz</p>
       </div>
     </footer>
     </>
