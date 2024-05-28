e8d14954218e80611ae70dc475259b00d6c8476a
Alessandro1809 <dalessandro18dg@gmail.com>
2024-05-28 11:40:49 -0600

    arreglando el slider y corrigiendo bug visual de categorias

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -7,8 +7,10 @@ const Slider = () => {
     <div className="wrapper max-h-1500px flex overflow-x-auto pt-16 pb-4 lg:pt-40 md:pt-40 mb-12">
         {data.data.map((item) =>(
             
-            <div key={item.id} className="item w-[110px] h-[110px] flex items-center justify-center bg-gray-300 rounded-full mr-4 transition-all duration-300 hover:scale-110">
-                <div className="item2 w-[100px] h-[100px] bg-black rounded-full ml-1 mr-1"></div>
+            <div key={item.id} className="item w-[110px] h-[110px] flex items-center justify-center bg-blue-400/50 rounded-full mr-4 transition-all duration-300 hover:scale-110">
+                <div className="item2 w-[100px] h-[100px] rounded-full ml-1 mr-1">
+                    <img src={item.img} alt="" className='w-[100px] h-[100px] rounded-full'/>
+                </div>
             </div>
         
         ))}
