e622c2aa41ec9a2be9247099f55033e5281586b0
Alessandro1809 <dalessandro18dg@gmail.com>
2024-06-01 16:53:25 -0600

    ajustando buscador para que vaya acorde al estilo

--- src/components/BodySearch.jsx
+++ src/components/BodySearch.jsx
@@ -4,7 +4,7 @@ const BodySearch = () => {
    <>
    <div className="p-4 mt-4 pb-10">
        <div className="lg:hidden justify-end w-full md:hidden sm:flex flex items-center">
-               <input type="text" className=" bg-gray-50 rounded-full p-2 hover:bg-white shadow-inner shadow-orange-400 w-full h-8 placeholder:text-sm placeholder:text-center outline-none" placeholder="Search recipes"/>
+               <input type="text" className=" bg-gray-50 rounded-full p-2 hover:bg-white shadow-inner shadow-orange-400 w-full h-10 placeholder:text-sm placeholder:text-center outline-none" placeholder="Search recipes"/>
                <button className="w-auto h-auto p-2 text-sm transition-all duration-200 md:w-12 hover:scale-105 md:text-xs ml-2 hover:bg-orange-100/90 rounded-full">
                  <Search/>
                </button>
--- src/components/Buscador.jsx
+++ src/components/Buscador.jsx
@@ -4,7 +4,7 @@ const Buscador = () => {
   return (
     <>
         <div className="lg:flex justify-end w-auto md:flex sm:hidden hidden items-center">
-              <input type="text" className=" bg-gray-50 rounded-full p-2 hover:bg-white shadow-inner shadow-orange-400 w-56 h-8 placeholder:text-sm placeholder:text-center outline-none" placeholder="Search recipes"/>
+              <input type="text" className=" bg-gray-50 rounded-full p-2 hover:bg-white shadow-inner shadow-orange-400 w-56 h-10 placeholder:text-sm placeholder:text-center outline-none" placeholder="Search recipes"/>
               
               <button className="border border-orange-400 w-auto h-auto p-2 text-sm transition-all duration-200 md:w-10 hover:scale-105 md:text-xs ml-3 bg-slate-100/50 hover:bg-orange-300/80 rounded-full">
                 <Search/>
