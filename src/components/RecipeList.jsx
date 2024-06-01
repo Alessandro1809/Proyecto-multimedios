0cff75b24f6c4f96f04bce71ccc59ba5e3772208
Alessandro1809 <dalessandro18dg@gmail.com>
2024-06-01 14:55:57 -0600

    agregando verificacion de eliminado de recetas de la lista de favoritos  mostrando en una ventana modal la desicion de eliminar o no, funcionalidad lista

--- src/components/RecipeList.jsx
+++ src/components/RecipeList.jsx
@@ -1,82 +1,105 @@
 import data from '../data/tipos.json'
 import { useState } from 'react';
-import { Favorite } from '@material-ui/icons';
+import { Favorite,Cancel } from '@material-ui/icons';
 import { useRecipe } from '../api/hooks/recipes';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+
 const RecipeList = () => {
+  const { agregarFavoritos, existeStorage, eliminarFavoritos } = useRecipe();
+  const [open, setOpen] = useState(false);
+  const [currentId, setCurrentId] = useState(null); // Estado para almacenar el id de cada receta que se quiera eliminar
 
-const {agregarFavoritos,existeStorage, eliminarFavoritos} = useRecipe();
+  const onOpenModal = (id) => {
+    setCurrentId(id); 
+    setOpen(true);
+  };
 
-const [open, setOpen] = useState(false);
-
-const onOpenModal = () => setOpen(true);
-const onCloseModal = () => setOpen(false);
+  const onCloseModal = () => {
+    setCurrentId(null); 
+    setOpen(false);
+  };
 
   const handleFavorite = (receta) => {
     existeStorage(receta.id);
     agregarFavoritos(receta);
-  }
+  };
 
-  const handleDelete = (id) => {
-    const resultado = window.confirm('¿Seguro que quieres eliminarlo de favoritos?');
-    if (resultado) { eliminarFavoritos(id); }
-    
-    return ;
-  }
+  const handleDelete = () => {
+    eliminarFavoritos(currentId); 
+    onCloseModal(); 
+  };
 
   return (
     <>
-              {/* <Modal open={open} onClose={onCloseModal} center>
-              <div className='p-6'>
-              <h2 className='text-xl font-medium'>¿Desea eliminar <span className="text-red-400 font-bold">permanentemente</span> de el elemento de favoritos?</h2>
-              </div>
-              <button onClick={(e) => handleDelete(item.id)} className='mx-2 my-4 w-auto h-auto p-2 text-lg bg-rose-400 transition-all duration-300 md:w-40 hover:bg-rose-400/80 border-b border-rose-400 md:text-lg rounded-lg'>Remove to {<Favorite />}</button>
-              </Modal> */}
-              
-    <main className='pb-5'>
+    <Modal open={open} onClose={onCloseModal} center>
+      <div className='p-6'>
+        <h2 className='text-xl font-medium'>
+          ¿Desea eliminar <span className="text-red-400 font-bold">permanentemente</span> el elemento de favoritos?
+        </h2>
+      </div>
+      <footer className='flex justify-center'>
+      <button
+        id='cancel'
+        onClick={onCloseModal}
+        className='delete mx-2 my-4 w-auto h-auto p-2 text-lg transition-all duration-300 md:w-40 hover:bg-cyan-400/80 border-b border-cyan-400 md:text-lg rounded-lg flex justify-center items-center'
+      >
+        Close &nbsp;<Cancel/> 
+      </button>
+      <button
+        id='delete'
+        onClick={handleDelete}
+        className='delete mx-2 my-4 w-auto h-auto p-2 text-lg bg-rose-400 transition-all duration-300 md:w-40 hover:bg-rose-400/80 border-b border-rose-400 md:text-lg rounded-lg'
+      >
+        Delete to {<Favorite/>}
+      </button>
       
-        <article className='grid justify-center pb-4 appear'>
-          <h2 className="text-5xl font-semibold text-center max-w-screen-xl pt-14 tracking-wider h-auto pb-4 text-pretty"><span className="text-orange-400">300+</span> Recipes</h2>
-          <p className=' text-xl font-medium tracking-wide mx-4 text-pretty'>Explore all the delicious recipes for <span className="text-orange-400">all tastes</span> in our catalog!</p>
-        </article>
-        
-        <section className='grid justify-center'>
-            <div>
-              <div className='p-4 grid gap-5 sm:gap-4 md:gap-6 grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 md:mx-0.5 col-auto'>
-              {data.data.map((item) =>(
-              
+      </footer>
+    </Modal>
+
+    <main className='pb-5'>
+      <article className='grid justify-center pb-4 appear'>
+        <h2 className="text-5xl font-semibold text-center max-w-screen-xl pt-14 tracking-wider h-auto pb-4 text-pretty">
+          <span className="text-orange-400">300+</span> Recipes
+        </h2>
+        <p className='text-xl font-medium tracking-wide mx-4 text-pretty'>
+          Explore all the delicious recipes for <span className="text-orange-400">all tastes</span> in our catalog!
+        </p>
+      </article>
+
+      <section className='grid justify-center'>
+        <div>
+          <div className='p-4 grid gap-5 sm:gap-4 md:gap-6 grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-4 md:mx-0.5 col-auto'>
+            {data.data.map((item) => (
               <article key={item.id} className="appear bg-gray-200 min-w-[150px] md:min-w-[180px] h-auto rounded-lg flex justify-center flex-col transition-all duration-600 hover:scale-100 hover:bg-gray-100 group drop-shadow-md">
-                  <a href="" className="group relative flex flex-col overflow-hidden rounded-t-lg px-4 pt-52 ">
+                <a href="" className="group relative flex flex-col overflow-hidden rounded-t-lg px-4 pt-52">
                   <img className="absolute inset-0 h-52 w-full object-cover group-hover:scale-105 transition-transform duration-500 ease-in-out" src={item.img} alt="" />
-                  </a>
-                  <article className='flex justify-between'>
-                      <p className="px-2 py-2 font-semibold text-black sm:text-lg md:text-xl tracking-wide">{item.nombre}</p>
-                      <div className='p-2'>
-                      
-                      </div>
-                  </article> 
-                      <p className="mx-2 mb-2 tracking-wide line-clamp-3 text-gray-800 font-medium text-pretty">{item.description}</p>
-                  <footer className='flex justify-between w-auto'>
-                  <button className='mx-2 my-4 w-auto h-auto p-2 text-lg  transition-all duration-300 md:w-40 hover:bg-orange-400 md:text-lg border border-orange-500 rounded-lg'>View Recipe</button>
+                </a>
+                <article className='flex justify-between'>
+                  <p className="px-2 py-2 font-semibold text-black sm:text-lg md:text-xl tracking-wide">{item.nombre}</p>
+                  <div className='p-2'></div>
+                </article>
+                <p className="mx-2 mb-2 tracking-wide line-clamp-3 text-gray-800 font-medium text-pretty">{item.description}</p>
+                <footer className='flex justify-between w-auto'>
+                  <button className='mx-2 my-4 w-auto h-auto p-2 text-lg transition-all duration-300 md:w-40 hover:bg-orange-400 md:text-lg border border-orange-500 rounded-lg'>View Recipe</button>
 
                   {existeStorage(item.id) ? 
-
-                  <button onClick={(e) => onOpenModal(e)} className='mx-2 my-4 w-auto h-auto p-2 text-lg bg-rose-400 transition-all duration-300 md:w-40 hover:bg-rose-400/80 border-b border-rose-400 md:text-lg rounded-lg'>Remove to {<Favorite />}</button> 
-                  : 
-                  <button onClick={(e) => handleFavorite({id:item.id,nombre:item.nombre,img:item.img,description:item.description})} className='mx-2 my-4 w-auto h-auto p-2 text-lg  transition-all duration-300 md:w-40 hover:bg-rose-400 border-b border-rose-400 md:text-lg rounded-lg'>Add to {<Favorite />}</button>}
-
-                  </footer>
+                    <button onClick={() => onOpenModal(item.id)} className='mx-2 my-4 w-auto h-auto p-2 text-lg bg-rose-400 transition-all duration-300 md:w-40 hover:bg-rose-400/80 border-b border-rose-400 md:text-lg rounded-lg'>
+                      Remove to {<Favorite />}
+                    </button>
+                    : 
+                    <button onClick={() => handleFavorite({ id: item.id, nombre: item.nombre, img: item.img, description: item.description })} className='mx-2 my-4 w-auto h-auto p-2 text-lg transition-all duration-300 md:w-40 hover:bg-rose-400 border-b border-rose-400 md:text-lg rounded-lg'>
+                      Add to {<Favorite />}
+                    </button>
+                  }
+                </footer>
               </article>
-        
-              ))}
-              
-              </div>
-            </div>
-        </section>
-           
-      </main>
-    </>
+            ))}
+          </div>
+        </div>
+      </section>
+    </main>
+  </>
   )
 }
 export default RecipeList;
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState   } from 'react';
 import { useRecipe } from '../api/hooks/recipes';
 import { CloseRounded } from '@material-ui/icons';
-
+import { Favorite, Cancel } from '@material-ui/icons';
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
 const Slider = () => {
   const { favs, actualizarFavoritos,eliminarFavoritos } = useRecipe();
+  const [open, setOpen] = useState(false);
+  const [currentId, setCurrentId] = useState(null);
+
+  const onOpenModal = (id) => {
+    setCurrentId(id); 
+    setOpen(true);
+  };
+
+  const onCloseModal = () => {
+    setCurrentId(null); 
+    setOpen(false);
+  };
+  const handleDelete = () => {
+    eliminarFavoritos(currentId); 
+    onCloseModal(); 
+  };
 
   // useEffect para actualizar favoritos cada vez que cambian
   useEffect(() => {
@@ -12,13 +30,37 @@ const Slider = () => {
   
   return (
     <>
+    <Modal open={open} onClose={onCloseModal} center>
+      <div className='p-6'>
+        <h2 className='text-xl font-medium'>
+          ¿Desea eliminar <span className="text-red-400 font-bold">permanentemente</span> el elemento de favoritos?
+        </h2>
+      </div>
+      <footer className='flex justify-center'>
+      <button
+        id='cancel'
+        onClick={onCloseModal}
+        className='delete mx-2 my-4 w-auto h-auto p-2 text-lg transition-all duration-300 md:w-40 hover:bg-cyan-400/80 border-b border-cyan-400 md:text-lg rounded-lg flex justify-center items-center'
+      >
+        Close &nbsp;<Cancel/> 
+      </button>
+      <button
+        id='delete'
+        onClick={handleDelete}
+        className='delete mx-2 my-4 w-auto h-auto p-2 text-lg bg-rose-400 transition-all duration-300 md:w-40 hover:bg-rose-400/80 border-b border-rose-400 md:text-lg rounded-lg'
+      >
+        Remove to {<Favorite/>}
+      </button>
+      </footer>
+    </Modal>
+
       {favs.length ? (
         <div className="wrapper max-h-1500px flex overflow-x-auto pt-16 pb-4 lg:pt-40 md:pt-40 mb-12 mx-2">
           {favs.map((item) => (
              
             <div key={item.id} className='justify-center items-center grid group'>
               <div>
-              <button className='md:flex hidden lg:flex transition-all duration-300 opacity-0 group-hover:opacity-100 hover:bg-red-200 rounded-full p-2 bg-slate-100' onClick={() => eliminarFavoritos(item.id)}><CloseRounded/></button>
+              <button className='md:flex hidden lg:flex transition-all duration-300 opacity-0 group-hover:opacity-100 hover:bg-red-200 rounded-full p-2 bg-slate-100' onClick={() => onOpenModal(item.id)}><CloseRounded/></button>
               </div>
               <div className="w-[110px] h-[110px] flex items-center justify-center bg-blue-400/50 rounded-full mx-2 transition-all duration-300 hover:scale-110">
                 <div className="item2 w-[100px] h-[100px] rounded-full mx-1">
