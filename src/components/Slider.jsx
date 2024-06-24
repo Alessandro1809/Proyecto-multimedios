4210d47a23781b3de1d19c9213353c22e79aa949
Alessandro1809 <dalessandro18dg@gmail.com>
2024-06-23 18:44:56 -0600

    ya funciona el redireccionaiento desde zona de favs a recetas

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -4,6 +4,7 @@ import { CloseRounded } from '@material-ui/icons';
 import { Favorite, Cancel } from '@material-ui/icons';
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
+import { Link } from 'react-router-dom';
 const Slider = () => {
   const { favs, actualizarFavoritos,eliminarFavoritos } = useRecipe();
   const [open, setOpen] = useState(false);
@@ -58,20 +59,23 @@ const Slider = () => {
         <div className="wrapper max-h-1500px flex overflow-x-auto pt-16 pb-4 lg:pt-40 md:pt-40 mb-12 mx-2">
           {favs.map((item) => (
              
-            <div key={item.id} className='justify-center items-center flex flex-col group'>
+           <div key={item.id} className='justify-center items-center flex flex-col group'>
               <div className='mb-2'>
               <button className='md:flex hidden lg:flex transition-all duration-300 opacity-0 group-hover:opacity-100 hover:bg-red-200 rounded-full p-2 bg-slate-100 ' onClick={() => onOpenModal(item.id)}><CloseRounded/></button>
               </div>
-              <div className="w-[110px] h-[110px] flex items-center justify-center bg-blue-400/50 rounded-full mx-2 transition-all duration-300 hover:scale-110">
+
+              <Link to={`/recipe/${item.id}`}>
+              <div className="w-[110px] h-[110px] grid items-center justify-center bg-blue-400/50 rounded-full mx-auto transition-all duration-300 hover:scale-110">
                 <div className="item2 w-[100px] h-[100px] rounded-full mx-1">
-                  <img src={item.img} alt="" className='w-[100px] h-[100px] rounded-full'/>
+                  <img src={item.img} alt="" className='w-[100px] h-[100px] rounded-full '/>
                 </div>
               </div>
               <p className="text-center mt-2 tracking-wide text-xs text-nowrap mx-2">
               {item.nombre}
               </p>
-             
+              </Link>
             </div>
+
           ))}
         </div>
       ) : (
